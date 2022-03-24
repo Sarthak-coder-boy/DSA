a6a1ea4f3c9821d342c02ee7597a3e3376550d35
Sarthak Gupta <gauravsarthak19@gmail.com>
2022-03-25 00:45:34 +0530

    binary search

---
+++ binary_search.cpp
@@ -0,0 +1,29 @@
+ 
+//  Elements should be in monotonc function i.e in sorted order
+
+#include<iostream>
+ using namespace std;
+ int bs(int a[],int size,int key){
+     int s=0,e=size-1;
+   int  mid=s+(e-s/2);
+   while(s<=e){
+       if(a[mid]==key)
+       return mid;
+       if(a[mid]>key)
+       e=mid-1;
+       else
+       s=mid+1;
+       mid=s+(e-s/2);
+   }
+   return -1;
+ }
+ int main (){
+  int n,key;
+  cin>>n>>key;
+  int a[n];
+  for(int i=0;i<n;i++){
+      cin>>a[i];
+  }
+  cout<<"At index "<<bs(a,n,key);
+  return 0;
+ }
